f9fcb0c7edbe4804dfc5ee88af2b410e79afa4e1
Ivan Repeta <ivan.repeta.work@gmail.com>
2020-11-20 21:55:15 +0200

    Add task #3 solution

---
+++ src/main/java/com/repeta/iasa/Task3.java
@@ -0,0 +1,14 @@
+package com.repeta.iasa;
+
+public class Task3 {
+    public static int digitalRoot(int n){
+        int res = String
+            .valueOf(n)
+            .chars()
+            .mapToObj(i -> new Integer(String.valueOf((char)i)))
+            .peek(System.out::println)
+            .reduce(0,(sum,digit)->sum+digit)
+        ;
+        return (res>9 ? digitalRoot(res) : res);
+    };
+}
---
+++ src/test/java/com/repeta/iasa/Task3Test.java
@@ -0,0 +1,15 @@
+package com.repeta.iasa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static com.repeta.iasa.Task3.digitalRoot;
+class Task3Test {
+    @Test
+    public void Should_ReturnDigitalRootOfNumber_When_NonNegativeValueIsPassed(){
+        assertEquals(7,digitalRoot(16));
+        //assertEquals(6,digitalRoot(924));
+        //assertEquals(6,digitalRoot(132189));
+        //assertEquals(2,digitalRoot(493193));
+    }
+}
