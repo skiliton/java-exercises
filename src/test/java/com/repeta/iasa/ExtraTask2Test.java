cdc09498f07ca32647fdbff6976e468663658e96
Ivan Repeta <ivan.repeta.work@gmail.com>
2020-11-21 17:14:08 +0200

    Add additional assertions to test getIPv4 method from ExtraTask2 class

--- src/test/java/com/repeta/iasa/ExtraTask2Test.java
+++ src/test/java/com/repeta/iasa/ExtraTask2Test.java
@@ -10,6 +10,8 @@ class ExtraTask2Test {
     @Test
     public void Should_ReturnIPv4AddressDerivedFromPassedNumber_When_NonNegativeNumberIsPassed(){
         assertEquals("128.32.10.1",getIPv4Address(2149583361L));
+        assertEquals("0.0.0.32",getIPv4Address(32L));
+        assertEquals("0.0.0.0",getIPv4Address(0L));
     }
 
 }
